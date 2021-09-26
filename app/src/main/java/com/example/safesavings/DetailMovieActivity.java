90edc699384bad7113ab50ebde9113bd08837bb4
hidayatullah191001 <hidayatullahd745@gmail.com>
2021-09-27 06:47:53 +0700

    add file

---
+++ app/src/main/java/com/example/safesavings/DetailMovieActivity.java
@@ -0,0 +1,13 @@
+package com.example.safesavings;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailMovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_movie);
+    }
+}
