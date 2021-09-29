c58f50f95f11b951d0c8986631b287cee7d71791
Liwinantia Salsabila <liwinantia1901@gmail.com>
2021-09-30 01:43:26 +0700

    Perbaikan Gradle

--- app/src/main/java/com/example/safesavings/MainActivity.java
+++ app/src/main/java/com/example/safesavings/MainActivity.java
@@ -1,78 +1,53 @@
 package com.example.safesavings;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.GridLayoutManager;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
-import android.view.Menu;
-import android.widget.SearchView;
+import android.view.MenuItem;
 
-import com.example.safesavings.adapter.LatestAnimeAdapter;
-import com.example.safesavings.adapter.SeasonAnimeAdapter;
-import com.example.safesavings.model.Latest;
-import com.example.safesavings.model.Response;
-import com.example.safesavings.model.Season;
-import com.example.safesavings.rest.ApiService;
-import com.example.safesavings.rest.ApiEndPoint;
-import com.takusemba.multisnaprecyclerview.MultiSnapRecyclerView;
-
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-
+import com.example.safesavings.fragment.FragmentAbout;
+import com.example.safesavings.fragment.FragmentHome;
+import com.example.safesavings.fragment.FragmentList;
+import com.example.safesavings.fragment.FragmentSearch;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private SeasonAnimeAdapter adapterSeason;
-    private LatestAnimeAdapter adapterLatest;
-    private SearchView searchView;
-    RecyclerView recyclerView1;
-    RecyclerView recyclerView2;
-    ApiEndPoint apiInterface;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottom_navigation_menu);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigation);
 
-
-        recyclerView1 = findViewById(R.id.rvSeason);
-        recyclerView1.setHasFixedSize(true);
-        recyclerView1.setLayoutManager(new LinearLayoutManager(MainActivity.this));
-
-        recyclerView2 = findViewById(R.id.rvLatest);
-
-        getDataFromSeason();
+        getSupportFragmentManager().beginTransaction().replace(R.id.container_fragment, new FragmentHome()).commit();
     }
 
-    private void getDataFromSeason() {
-        ApiService.endPoint().getDataSeason()
-                .enqueue(new Callback<Response>() {
-                    @Override
-                    public void onResponse(Call<Response> call, retrofit2.Response<Response> response) {
-                        if (response.isSuccessful()){
-                            List<Season> mSeason = response.body().getSeason();
-                            adapterSeason = new SeasonAnimeAdapter(MainActivity.this, mSeason);
-                            recyclerView1.setAdapter(adapterSeason);
-                            List<Latest> mLatest = response.body().getLatest();
-                            adapterLatest = new LatestAnimeAdapter(MainActivity.this, mLatest);
-                            recyclerView2.setAdapter(adapterLatest);
-                        }
-                    }
-                    @Override
-                    public void onFailure(Call<Response> call, Throwable t) {
-
-                }
-        });
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main_menu, menu);
-        return super.onCreateOptionsMenu(menu);
-    }
+    private BottomNavigationView.OnNavigationItemSelectedListener navigation = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected( MenuItem item) {
+            Fragment f =  null;
+            switch (item.getItemId()){
+                case R.id.menu_home:
+                    f = new FragmentHome();
+                    break;
+                case R.id.menu_search:
+                    f = new FragmentSearch();
+                    break;
+                case R.id.menu_list:
+                    f = new FragmentList();
+                    break;
+                case R.id.menu_about:
+                    f = new FragmentAbout();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.container_fragment,f).commit();
+            return true;
+        }
+    };
 }
---
+++ app/src/main/java/com/example/safesavings/fragment/FragmentHome.java
@@ -0,0 +1,72 @@
+package com.example.safesavings.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.SearchView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.safesavings.MainActivity;
+import com.example.safesavings.R;
+import com.example.safesavings.adapter.LatestAnimeAdapter;
+import com.example.safesavings.adapter.SeasonAnimeAdapter;
+import com.example.safesavings.model.Latest;
+import com.example.safesavings.model.Response;
+import com.example.safesavings.model.Season;
+import com.example.safesavings.rest.ApiEndPoint;
+import com.example.safesavings.rest.ApiService;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+
+public class FragmentHome extends Fragment {
+    private SeasonAnimeAdapter adapterSeason;
+    private LatestAnimeAdapter adapterLatest;
+    private SearchView searchView;
+    RecyclerView recyclerView1;
+    RecyclerView recyclerView2;
+    ApiEndPoint apiInterface;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        recyclerView1 = view.findViewById(R.id.rvSeason);
+        recyclerView1.setHasFixedSize(true);
+        recyclerView1.setLayoutManager(new LinearLayoutManager(view.getContext()));
+        recyclerView2 = view.findViewById(R.id.rvLatest);
+        getDataFromSeason();
+        return view;
+    }
+    private void getDataFromSeason() {
+        ApiService.endPoint().getDataSeason()
+            .enqueue(new Callback<Response>() {
+                @Override
+                public void onResponse(Call<Response> call, retrofit2.Response<Response> response) {
+                    if (response.isSuccessful()){
+                        List<Season> mSeason = response.body().getSeason();
+                        adapterSeason = new SeasonAnimeAdapter(getActivity(),mSeason);
+                        recyclerView1.setAdapter(adapterSeason);
+                        List<Latest> mLatest = response.body().getLatest();
+                        adapterLatest = new LatestAnimeAdapter(getActivity(), mLatest);
+                        recyclerView2.setAdapter(adapterLatest);
+                    }
+                }
+                @Override
+                public void onFailure(Call<Response> call, Throwable t) {
+
+                }
+            });
+    }
+}
